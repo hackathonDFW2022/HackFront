8e0089aedd276ac110a04b5133f6018e39b7c740
Forehand, Dayton <daytonforehand@my.unt.edu>
2022-09-24 11:20:11 -0500

    new new

---
+++ src/pages/Form.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+      <form action="">
+        <div className='relative z-0 mb-6 w-full group'>
+            <input />
+            <label />
+        </div>
+        <div>
+
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Form
