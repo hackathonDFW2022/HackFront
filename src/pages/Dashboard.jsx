0f24380fc9ca42f7625c502d776bfb2b373ebe01
Kobe Edmond <e.kobedevon@gmail.com>
2022-09-25 04:58:14 -0500

    dashboard basics

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Card from '../components/Card'
 import LineChart from '../components/LineChart'
+
+import {BsLightbulb} from "react-icons/bs"
 const Dashboard = () => {
   return (
 
@@ -14,9 +16,47 @@ const Dashboard = () => {
             Leaderboard
           </div>
           <div className="w-4/12 border ">
-            Tips
+            <div className="flex justify-center items-center bg-green-400 m-3 p-3 rounded-xl">
+              <div className="">
+                <h3 className='font-bold'>Here comes the sun</h3>
+                <p>During daylight hours, switch off artificial lights and use windows and skylights to brighten your home.</p>
+              </div>
+              <BsLightbulb className='text-6xl'/>
+            </div>
+            <div className="flex justify-center items-center bg-green-400 m-3 p-3 rounded-xl">
+              <div className="">
+                <h3 className='font-bold'>Caulk air leaks</h3>
+                <p>Using low-cost caulk to seal cracks and openings in your home keeps warm air out -- and cash in your wallet.</p>
+              </div>
+              <BsLightbulb className='text-6xl'/>
+            </div>
           </div>
-          <div className="w-4/12 border ">Recomended Friends</div>
+          <div className="w-4/12 flex flex-col justify-center items-center">
+            <h1 className='text-2xl font-bold'>Recommended Friends</h1>
+            <div className=" flex px-16 py-4 justify-between gap-24">
+              <div className="flex flex-col justify-center items-center">
+                <div className="inline-flex overflow-hidden relative justify-center items-center w-24 h-24 bg-green-500 rounded-full">
+                  <span className="text-5xl text-green-700">JG</span>
+                </div>
+                <h2 className='underline'>Julian Garcia</h2>
+                <h3 className='font-bold text-sm'>Add+</h3>
+              </div>
+              <div className="flex flex-col justify-center items-center">
+                <div className="inline-flex overflow-hidden relative justify-center items-center w-24 h-24 bg-green-500 rounded-full">
+                  <span className="text-5xl text-green-700">DF</span>
+                </div>
+                <h2 className='underline'>Dayton Forehand</h2>
+                <h3 className='font-bold text-sm'>Add+</h3>
+              </div>
+              <div className="flex flex-col justify-center items-center">
+                <div className="inline-flex overflow-hidden relative justify-center items-center w-24 h-24 bg-green-500 rounded-full">
+                  <span className="text-5xl text-green-700">JB</span>
+                </div>
+                <h2 className='underline'>Jacob Benz</h2>
+                <h3 className='font-bold text-sm'>Add+</h3>
+              </div>
+          </div>
+          </div>
           <div className="w-4/12 border ">Other</div>
           <div className="w-4/12 border ">other</div>
         </div>
