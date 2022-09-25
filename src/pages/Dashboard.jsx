d586589c7906b8ccb72e8e99cf91fba008b62adf
Kobe Edmond <e.kobedevon@gmail.com>
2022-09-25 04:32:52 -0500

    basic layout for dash

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,6 +5,20 @@ const Dashboard = () => {
   return (
     <>
         <Card/>
+        <div className="border flex flex-wrap">
+          <div className="w-4/12 border ">
+            Results
+          </div>
+          <div className="w-4/12 border ">
+            Leaderboard
+          </div>
+          <div className="w-4/12 border ">
+            Tips
+          </div>
+          <div className="w-4/12 border ">Recomended Friends</div>
+          <div className="w-4/12 border ">Other</div>
+          <div className="w-4/12 border ">other</div>
+        </div>
     </>
   )
 }
