956816c260cf15fa28d6dd5a02d0ec2077f8f5e5
Kobe Edmond <e.kobedevon@gmail.com>
2022-09-25 05:52:58 -0500

    Upload Dash

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -2,40 +2,40 @@ import React from 'react'
 
 export default function Leaderboard() {
   return (
-    <div>
-        <div className="font-bold">
-            Leaderboard
+    <div className='bg-green-400 p-3 m-3 rounded-2xl'>
+        <div className="font-bold text-center">
+            <h1 className='text-2xl'>Leaderboard</h1>
         </div>
-        <div className="flex flex-wrap">
-            <div className="text-grey-300 w-4/12 text-sm">USERNAME</div>
-            <div className="text-grey-300 w-4/12 text-sm">SCORE</div>
-            <div className="text-grey-300 w-4/12 text-sm">REGION</div>
+        <div className="flex flex-wrap font-bold border-b-2 border-black/50">
+            <div className="text-grey-300 w-4/12 ">USERNAME</div>
+            <div className="text-grey-300 w-4/12 ">SCORE</div>
+            <div className="text-grey-300 w-4/12 text-s">REGION</div>
         </div>
         <div>
-            <div className="flex flex-wrap">
+            <div className="flex flex-wrap border-b-2 border-black/50">
                 <div className="w-4/12 text-grey-400">juls9301</div>
-                <div className="w-4/12 text-grey-400">87%</div>
-                <div className="w-4/12 text-grey-400">Texas</div>
+                <div className="w-4/12 text-grey-400">80%</div>
+                <div className="w-4/12 text-grey-400">DFW</div>
             </div>
-            <div className="flex flex-wrap">
+            <div className="flex flex-wrap border-b-2 border-black/50">
                 <div className="w-4/12 text-grey-400">user3421</div>
-                <div className="w-4/12 text-grey-400">87%</div>
-                <div className="w-4/12 text-grey-400">Texas</div>
+                <div className="w-4/12 text-grey-400">83%</div>
+                <div className="w-4/12 text-grey-400">DFW</div>
             </div>
-            <div className="flex flex-wrap">
+            <div className="flex flex-wrap border-b-2 border-black/50">
                 <div className="w-4/12 text-grey-400">jakobeus</div>
-                <div className="w-4/12 text-grey-400">87%</div>
-                <div className="w-4/12 text-grey-400">Texas</div>
+                <div className="w-4/12 text-grey-400">86%</div>
+                <div className="w-4/12 text-grey-400">DFW</div>
             </div>
-            <div className="flex flex-wrap">
+            <div className="flex flex-wrap border-b-2 border-black/50">
                 <div className="w-4/12 text-grey-400">kobedevon</div>
                 <div className="w-4/12 text-grey-400">87%</div>
-                <div className="w-4/12 text-grey-400">Texas</div>
+                <div className="w-4/12 text-grey-400">DFW</div>
             </div>
-            <div className="flex flex-wrap">
+            <div className="flex flex-wrap border-b-2 border-black/50">
                 <div className="w-4/12 text-grey-400">daytoncf</div>
-                <div className="w-4/12 text-grey-400">87%</div>
-                <div className="w-4/12 text-grey-400">Texas</div>
+                <div className="w-4/12 text-grey-400">92%</div>
+                <div className="w-4/12 text-grey-400">DFW</div>
             </div>         
         </div>
     </div>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
+import Leaderboard from "../components/Leaderboard"
 
 
 import {BsLightbulb} from "react-icons/bs"
 const Dashboard = () => {
   return (
 
-    <>
-        
+    <div className='bg-green-100 h-screen'>
         <div className="border flex flex-wrap">
           <div className="w-4/12 flex flex-col items-center ">
             <h1 className='text-2xl font-bold'>Results</h1>
@@ -17,26 +17,16 @@ const Dashboard = () => {
               <p className='text-green-800 text-xl'>Your carbon footprint is slighty below average!</p>
             </div>
           </div>
+          <div className="w-4/12 border text-center ">
+             <h1 className='text-2xl font-bold'>Breakdown</h1>
+             <div className="pt-4 text-xl m-4">
+              <p>Your key carbon emmiters are <span className='font-bold'>Electricity & Vehicle Emmissions</span> which contribute 50% of your carbon footprint</p>
+             </div>
+          </div>
           <div className="w-4/12 border ">
             <Leaderboard/>
           </div>
-          <div className="w-4/12 border ">
-            <div className="flex justify-center items-center bg-green-400 m-3 p-3 rounded-xl">
-              <div className="">
-                <h3 className='font-bold'>Here comes the sun</h3>
-                <p>During daylight hours, switch off artificial lights and use windows and skylights to brighten your home.</p>
-              </div>
-              <BsLightbulb className='text-6xl'/>
-            </div>
-            <div className="flex justify-center items-center bg-green-400 m-3 p-3 rounded-xl">
-              <div className="">
-                <h3 className='font-bold'>Caulk air leaks</h3>
-                <p>Using low-cost caulk to seal cracks and openings in your home keeps warm air out -- and cash in your wallet.</p>
-              </div>
-              <BsLightbulb className='text-6xl'/>
-            </div>
-          </div>
-          <div className="w-4/12 flex flex-col justify-center items-center">
+          <div className="w-6/12 flex flex-col justify-center items-center">
             <h1 className='text-2xl font-bold'>Recommended Friends</h1>
             <div className=" flex px-16 py-4 justify-between gap-24">
               <div className="flex flex-col justify-center items-center">
@@ -50,7 +40,7 @@ const Dashboard = () => {
                 <div className="inline-flex overflow-hidden relative justify-center items-center w-24 h-24 bg-green-500 rounded-full">
                   <span className="text-5xl text-green-700">DF</span>
                 </div>
-                <h2 className='underline'>Dayton Forehand</h2>
+                <h2 className='underline'>Dayton Forehan</h2>
                 <h3 className='font-bold text-sm'>Add+</h3>
               </div>
               <div className="flex flex-col justify-center items-center">
@@ -62,10 +52,27 @@ const Dashboard = () => {
               </div>
           </div>
           </div>
-          <div className="w-4/12 border ">Other</div>
-          <div className="w-4/12 border ">other</div>
+          
+          <div className="w-6/12 border ">
+            <div className="flex justify-between items-center bg-green-400 m-3 p-3 rounded-xl">
+              <div className="">
+                <h3 className='font-bold'>Here comes the sun</h3>
+                <p>During daylight hours, switch off artificial lights and use windows and skylights to brighten your home.</p>
+              </div>
+              <BsLightbulb className='text-6xl'/>
+            </div>
+            
+            <div className="flex justify-between items-center bg-green-400 m-3 p-3 rounded-xl">
+              <div className="">
+                <h3 className='font-bold'>Caulk air leaks</h3>
+                <p>Using low-cost caulk to seal cracks and openings in your home keeps warm air out -- and cash in your wallet.</p>
+              </div>
+              <BsLightbulb className='text-6xl'/>
+            </div>
+          </div>
+          
         </div>
-    </>
+    </div>
 
   )
 }
